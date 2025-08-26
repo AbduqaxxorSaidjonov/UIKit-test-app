3da8b955a3114277129d2500846689a223e8ed86
Abduqaxxor <abduqaxxor.saidjonov@ssd.uz>
2025-08-26 08:45:17 +0500

    Initial Commit

---
+++ UIKit-test-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-test-app
+//
+//  Created by Abduqaxxor Saidjonov on 26/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
